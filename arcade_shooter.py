d30d0fe776b0e4fe03b72b76fefd5677feeeb4cd
knoriega <nigel.noriega@xmonetaecapital.com>
2020-01-18 19:47:05 -0500

    collision timer and looping music

--- arcade_shooter.py
+++ arcade_shooter.py
@@ -40,9 +40,6 @@ class SpaceShooter(arcade.Window):
         """Initialize the game"""
         super().__init__(width, height, title)
 
-        # Start game not paused
-        self.paused = False
-
         # Set up the empty sprite lists
         # SpriteList has a lot of convenient methods
         #   .draw() for drawing all sprites in list
@@ -70,11 +67,31 @@ class SpaceShooter(arcade.Window):
         # Spawn new cloud every second -- define self.add_cloud!
         arcade.schedule(self.add_cloud, 1.0)
 
+        # Load bkg music
+        # Sound source: http://ccmixter.org/files/Apoxode/59262
+        # License: https://creativecommons.org/licenses/by/3.0/
+        self.background_music = arcade.load_sound(
+            "sounds/Apoxode_-_Electric_1.wav"
+        )
+
         # Load in sounds
         # Sounds sources: Jon Fincher
         self.collision_sound = arcade.load_sound('sounds/Collision.wav')
         self.move_up_sound = arcade.load_sound('sounds/Rising_putter.wav')
         self.move_down_sound = arcade.load_sound('sounds/Falling_putter.wav')
+
+        # Start the background music
+        arcade.play_sound(self.background_music)
+
+        # Start game unpaused
+        self.paused = False
+
+        # Collision timer to play sound
+        self.collided = False
+        self.collision_timer = 0.0
+
+        # Bkg music timer to loop music
+        self.bkg_music_timer = 0.0
 
     def on_draw(self):
         """Draw all game objects"""
@@ -89,6 +106,17 @@ class SpaceShooter(arcade.Window):
             delta_time {float} -- Time since last update
         """
 
+        # Did we collided with anything earlier? If so, update timer
+        if self.collided:
+            self.collision_timer += delta_time
+
+            # If the window has stayed open for 1 second, close it
+            if self.collision_timer > 1.0:
+                arcade.close_window()
+
+            # Stop updating once we've collided
+            return
+
         # If paused don't do anything
         if self.paused:
             return
@@ -96,11 +124,19 @@ class SpaceShooter(arcade.Window):
         # Did player hit anything? If so, end the game!
         #   Subtlety: check for collision before drawing anything
         if len(self.player.collides_with_list(self.enemies_list)) > 0:
+            self.collided = True
+            self.collision_timer = 0.0
             arcade.play_sound(self.collision_sound)
-            arcade.close_window()
 
-        # Update everything to show movement!
-        self.all_sprites.update()
+        # Update positions manually to account for framerate
+        for sprite in self.all_sprites:
+            sprite.center_x = int(
+                sprite.center_x + sprite.change_x * delta_time
+            )
+
+            sprite.center_y = int(
+                sprite.center_y + sprite.change_y * delta_time
+            )
 
         # Keep player on screen
         if self.player.top > self.height:
@@ -111,6 +147,13 @@ class SpaceShooter(arcade.Window):
             self.player.bottom = 0
         if self.player.left < 0:
             self.player.left = 0
+
+        # Increment bkg music timer and loop it if we need to
+        self.bkg_music_timer += delta_time
+
+        if self.bkg_music_timer > 15:
+            self.bkg_music_timer = 0.0
+            arcade.play_sound(self.background_music)
 
     def on_key_press(self, symbol, modifiers):
         """Handle user keyboard input
@@ -132,18 +175,18 @@ class SpaceShooter(arcade.Window):
             self.paused = not self.paused
 
         if symbol == arcade.key.I or symbol == arcade.key.UP:
-            self.player.change_y = 5
+            self.player.change_y = 250
             arcade.play_sound(self.move_up_sound)
 
         if symbol == arcade.key.J or symbol == arcade.key.LEFT:
-            self.player.change_x = -5
+            self.player.change_x = -250
 
         if symbol == arcade.key.K or symbol == arcade.key.DOWN:
-            self.player.change_y = -5
+            self.player.change_y = -250
             arcade.play_sound(self.move_down_sound)
 
         if symbol == arcade.key.L or symbol == arcade.key.RIGHT:
-            self.player.change_x = 5
+            self.player.change_x = 250
 
     def on_key_release(self, symbol, modifiers):
         """Undo movement vectors when movement keys are released
@@ -176,6 +219,10 @@ class SpaceShooter(arcade.Window):
             delta_time {float} -- How much time has passed since last call (required by arcade.schedule)
         """
 
+        # Don't add an enemy if the game is paused
+        if self.paused:
+            return
+
         # 1. Create new enemy sprite
         enemy = FlyingSprite('images/missile.png', SCALING)
 
@@ -184,7 +231,7 @@ class SpaceShooter(arcade.Window):
         enemy.top = random.randint(10, self.height - 10)
 
         # 3. Set movement speed -- motion attributes can handle movements during update() calls
-        enemy.velocity = (random.randint(-20, -5), 0)
+        enemy.velocity = (random.randint(-200, -50), 0)
 
         # 4. Add it to the enemies and all_sprites list
         self.enemies_list.append(enemy)
@@ -196,6 +243,11 @@ class SpaceShooter(arcade.Window):
         Args:
             delta_time {float} -- How much time has passed since last call (required by arcade.schedule)
         """
+
+        # Don't add cloud if the game is paused
+        if self.paused:
+            return
+
         # 1. Create cloud
         cloud = FlyingSprite('images/cloud.png')
 
@@ -204,7 +256,7 @@ class SpaceShooter(arcade.Window):
         cloud.top = random.randint(10, self.height - 10)
 
         # 3. Set speed to a random slow speed heading left
-        cloud.velocity = (random.randint(-5, -2), 0)
+        cloud.velocity = (random.randint(-50, -20), 0)
 
         # 4.Add it to the enemies list
         self.clouds_list.append(cloud)
